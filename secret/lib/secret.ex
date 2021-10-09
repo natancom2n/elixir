cdf4515ec896baeb26f6486f2bf10a9af760ad2a
natancom2n <natancom2n@gmail.com>
2021-10-09 11:11:32 -0300

    study project to exersim.org

---
+++ secret/lib/secret.ex
@@ -0,0 +1,18 @@
+defmodule Secret do
+  @moduledoc """
+  Documentation for Secret.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Secret.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
